61b8552f5f7fa6b006110b1b7affd79860f3e03a
JONATHAN SU <70356797+9661328@users.noreply.github.com>
2021-04-24 22:23:26 -0600

    Modified

--- src/Java/Question Asker/Main.java
+++ src/Java/Question Asker/Main.java
@@ -1,7 +1,7 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
-import java.util.ArrayList;
 
 class Main {
   public static void main(String[] args) {
@@ -9,36 +9,37 @@ class Main {
       File inputFile = new File("Questions.txt");
       File outputFile = new File("Output.txt");
       PrintWriter pw = new PrintWriter(new FileWriter(outputFile, true));
+      ArrayList < String > questions = new ArrayList < String > ();
       Scanner fileScanner = new Scanner(inputFile);
       Scanner inputScanner = new Scanner(System.in);
       Random random = new Random();
-      ArrayList < String > questions = new ArrayList < String > ();
-      boolean run = true;
+      boolean run;
+      int questionNumber;
+      String answer, again;
 
       while (fileScanner.hasNextLine()) {
-        String next = fileScanner.nextLine();
-        questions.add(next);
+        questions.add(fileScanner.nextLine());
       }
 
       System.out.println("Open-ended questions attempt to make the person who is answering the question give a more detailed and elaborate response. While almost any question can be answered in a few words, if you try hard enough, a good open-ended question tries to get around this.\n");
+
       do {
-        int questionNumber = random.nextInt(questions.size() + 1);
+        questionNumber = random.nextInt(questions.size());
         System.out.println(questions.get(questionNumber));
-        String answer = inputScanner.nextLine();
+        answer = inputScanner.nextLine();
         pw.println(questions.get(questionNumber));
         pw.println(answer + "\n");
-        System.out.println("\nDo you want another question? Type 'y' for yes and 'n' for no.");
-        String again = inputScanner.nextLine();
+        System.out.println("\nDo you want another question? Type 'y' for yes and any other key for no.");
+        again = inputScanner.nextLine();
+        run = again.equals("y") || again.equals("Y") ? true : false;
         System.out.println("");
-        if (again.equals("y")) {
-          run = true;
-        } else if (again.equals("n")) {
-          System.out.println("Saving your responses...done.");
-          run = false;
-          pw.close();
-          fileScanner.close();
-        }
       } while (run);
+
+      System.out.println("Saving your responses...done.");
+      pw.println("~~~~~~~~~~~~~~~\n");
+      pw.close();
+      fileScanner.close();
+      inputScanner.close();
     } catch (IOException e) {
       System.out.println("An error has occured.");
     }
